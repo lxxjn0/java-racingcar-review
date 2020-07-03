2d1eb20490e124056cd4e2f33b333798ef05ee9e
lxxjn0 <ljy3510@gmail.com>
2020-07-03 18:29:22 +0900

    feat : 자동차 이름의 길이가 5글자 이하만 가능하도록 하는 기능 구현
    - 자동차 이름을 감싸는 Name 객체 구현.
    - 자동차 이름에 대한 유효성 검증 기능 구현.

---
+++ src/main/java/me/lxxjn0/car/domain/Name.java
@@ -0,0 +1,25 @@
+package me.lxxjn0.car.domain;
+
+public class Name {
+
+	private static final int NAME_LENGTH_LOWER_BOUND = 1;
+	private static final int NAME_LENGTH_UPPER_BOUND = 5;
+
+	private final String name;
+
+	public Name(final String name) {
+		validate(name);
+		this.name = name;
+	}
+
+	private void validate(final String name) {
+		if (null == name) {
+			throw new NullPointerException("자동차 이름이 null입니다.");
+		}
+
+		if (name.length() < NAME_LENGTH_LOWER_BOUND || name.length() > NAME_LENGTH_UPPER_BOUND) {
+			throw new IllegalArgumentException("자동차 이름이 유효하지 않습니다.");
+		}
+	}
+
+}
