a376ac8c748d81b78ce8a2cc4cc286ed331091ed
lxxjn0 <ljy3510@gmail.com>
2020-07-19 19:21:47 +0900

    feat: 몇 번의 이동을 할 것인지 입력받는 기능 구현

--- src/main/java/me/lxxjn0/view/ConsoleInput.java
+++ src/main/java/me/lxxjn0/view/ConsoleInput.java
@@ -9,8 +9,13 @@ public class ConsoleInput {
     private ConsoleInput() {
     }
 
-    public static String inputCarNames() {
+    public static String receiveCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)");
+        return SCANNER.nextLine();
+    }
+
+    public static String receiveAttemptNumber() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
         return SCANNER.nextLine();
     }
 }
